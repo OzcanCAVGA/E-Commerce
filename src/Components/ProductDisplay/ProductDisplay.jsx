545b19e1b6e6e6282cb84e1f96bb79f02f7b7696
Özcan ÇAVGA <81030695+OzcanCAVGA@users.noreply.github.com>
2024-03-11 15:35:05 +0300

    Fix import paths and console.log statements

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,56 +1,56 @@
 import React from 'react'
-import './ProductDisplay'
+import './ProductDisplay.css'
 import star_icon from '../Assets/star_icon.png'
 import star_dull_icon from '../Assets/star_dull_icon.png'
 
-function ProductDisplay(props) {
-    const {product} = props
-  return (
-    <div className='productdisplay'>
-        <div className="productdisplay-left">
-            <div className="productdisplay-img-list">
-                <img src={props.image} alt="" />
-                <img src={props.image} alt="" />
-                <img src={props.image} alt="" />
-                <img src={props.image} alt="" />
-            </div>
-            <div className="productdisplay-img">
-                <img className='productdisplay-main-img' src={product.image} alt="" />
-            </div>
-        </div>
-        <div className="productdisplay-right">
-            <h1>{product.name}</h1>
-            <div className="productdisplay-right-star">
-                <img src={star_icon} alt="" />
-                <img src={star_icon} alt="" />
-                <img src={star_icon} alt="" />
-                <img src={star_icon} alt="" />
-                <img src={star_dull_icon} alt="" />
-                <p>(122)</p>
-            </div>
-            <div className="productdisplay-right-prices">
-                <div className="productdisplay-rigt-price-old">${product.old_price}</div>
-                <div className="productdisplay-rigt-price-old">${product.new_price}</div>
-            </div>
-            <div className="productdisplay-right-description">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est dolorum earum sint, alias distinctio error tempora voluptate atque! Reprehenderit numquam rerum quibusdam corporis, hic placeat quos veniam unde adipisci, excepturi maiores explicabo modi tempora reiciendis!
-            </div>
-            <div className="productdisplay-right-size">
-                <h1>Select Size</h1>
-                <div className="productdisplay-right-size">
-                    <div>S</div>
-                    <div>M</div>
-                    <div>L</div>
-                    <div>XL</div>
-                    <div>XXl</div>
+const ProductDisplay = ({product}) => {
+    console.log("ben buradayim", product)
+    return (
+        <div className='productdisplay'>
+            <div className="productdisplay-left">
+                <div className="productdisplay-img-list">
+                    <img src={product.image} alt="" />
+                    <img src={product.image} alt="" />
+                    <img src={product.image} alt="" />
+                    <img src={product.image} alt="" />
+                </div>
+                <div className="productdisplay-img">
+                    <img className='productdisplay-main-img' src={product.image} alt="" />
                 </div>
             </div>
-            <button>ADD TO CART</button>
-            <p className='productdisplay-right-category'><span>Category: </span>Women, T-Shirt, Crop Top</p>
-            <p className='productdisplay-right-category'><span>Tags </span>Modern, Latest</p>
+            <div className="productdisplay-right">
+                <h1>{product.name}</h1>
+                <div className="productdisplay-right-star">
+                    <img src={star_icon} alt="" />
+                    <img src={star_icon} alt="" />
+                    <img src={star_icon} alt="" />
+                    <img src={star_icon} alt="" />
+                    <img src={star_dull_icon} alt="" />
+                    <p>(122)</p>
+                </div>
+                <div className="productdisplay-right-prices">
+                    <div className="productdisplay-rigt-price-old">${product.old_price}</div>
+                    <div className="productdisplay-rigt-price-old">${product.new_price}</div>
+                </div>
+                <div className="productdisplay-right-description">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est dolorum earum sint, alias distinctio error tempora voluptate atque! Reprehenderit numquam rerum quibusdam corporis, hic placeat quos veniam unde adipisci, excepturi maiores explicabo modi tempora reiciendis!
+                </div>
+                <div className="productdisplay-right-size">
+                    <h1>Select Size</h1>
+                    <div className="productdisplay-right-size">
+                        <div>S</div>
+                        <div>M</div>
+                        <div>L</div>
+                        <div>XL</div>
+                        <div>XXl</div>
+                    </div>
+                </div>
+                <button>ADD TO CART</button>
+                <p className='productdisplay-right-category'><span>Category: </span>Women, T-Shirt, Crop Top</p>
+                <p className='productdisplay-right-category'><span>Tags </span>Modern, Latest</p>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default ProductDisplay
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -3,16 +3,20 @@ import { ShopContext } from '../Context/ShopContext'
 import { useParams } from 'react-router-dom'
 import Breadcrum from '../Components/Breadcrums/Breadcrum'
 import ProductDisplay from '../Components/ProductDisplay/ProductDisplay'
+
 export const Product = () => {
   const { all_product } = useContext(ShopContext)
   const { productId } = useParams();
-  const product = all_product.find((e) => {
-    e.id === Number(productId)
-  })
+  console.log(" Product Id ",productId)
+  console.log("All products: ",all_product)
+  const product = all_product.find((product) => product.id == productId);
+  console.log("Find ile ciktifirst", product)
+
+
   return (
     <div>
-      <Breadcrum props={product} />
-      <ProductDisplay props={product}/>
+      <Breadcrum product={product} />
+      <ProductDisplay product={product} />
     </div>
   )
 }
